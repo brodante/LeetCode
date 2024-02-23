267bc928e4719683d22ca1e2ea0e06ff01422cee
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-02-24 01:13:32 +0530

    Time: 156 ms (5.25%) | Memory: 53 MB (5.12%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
+    {
+        vector<int> c(n,1e5);
+        c[src]=0;
+        for(int z=0;z<=k;++z)
+        {
+            vector<int> C(c);
+            for(auto e:flights)
+                C[e[1]]=min(C[e[1]],c[e[0]]+e[2]);
+            c=C;
+        }
+        return c[dst]==1e5?-1:c[dst];
+    }
+};
