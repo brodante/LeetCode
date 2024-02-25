800bb49d1febe77de91c64773e6b9c914c22c40c
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-02-25 07:40:54 +0530

    Time: 156 ms (5.00%) | Memory: 53 MB (5.31%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -7,9 +7,9 @@ public:
         for(int i=0;i<=k;++i)
         {
             vector<int> tmp(ans);
-            for(auto e:flights)
-                if(ans[e[0]]!=INT_MAX)
-                    tmp[e[1]]=min(tmp[e[1]],ans[e[0]]+e[2]);
+            for(auto it:flights)
+                if(ans[it[0]]!=INT_MAX)
+                    tmp[it[1]]=min(tmp[it[1]],ans[it[0]]+it[2]);
             ans=tmp;
         }
         return ans[dst]==INT_MAX?-1:ans[dst];
