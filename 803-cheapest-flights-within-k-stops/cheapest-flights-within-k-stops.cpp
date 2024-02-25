65cf5b7ead48812ab6550503a1f4cd50de3bbe3c
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-02-25 07:22:17 +0530

    Time: 163 ms (5.00%) | Memory: 53.1 MB (5.31%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,17 +1,17 @@
-const int MY_INFINITY=1e5;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
     {
-        vector<int> ans(n,MY_INFINITY);
+        vector<int> ans(n,INT_MAX);
         ans[src]=0;
         for(int i=0;i<=k;++i)
         {
             vector<int> tmp(ans);
             for(auto e:flights)
-                tmp[e[1]]=min(tmp[e[1]],ans[e[0]]+e[2]);
+                if(ans[e[0]]!=INT_MAX)
+                    tmp[e[1]]=min(tmp[e[1]],ans[e[0]]+e[2]);
             ans=tmp;
         }
-        return ans[dst]==MY_INFINITY?-1:ans[dst];
+        return ans[dst]==INT_MAX?-1:ans[dst];
     }
 };
