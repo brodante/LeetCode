947111a42c965aec5f0fac4f86dd298eda54a35a
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-03-01 08:07:25 +0530

    Time: 4 ms (61.13%) | Memory: 9 MB (19.34%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -2,15 +2,7 @@ class Solution {
 public:
     string maximumOddBinaryNumber(string s)
     {
-        int hashh[2];
-        for(char it:s)
-            ++hashh[it-'0'];        
-        string ans;
-        for(int i=0;i<hashh[1]-1;++i)
-            ans+='1';
-        for(int i=0;i<hashh[0];++i)
-            ans+='0';
-        ans+='1';
-        return ans;
+        int n=count(s.begin(),s.end(),'1');
+        return string(n-1,'1')+string(s.size()-n,'0')+'1';
     }
 };
