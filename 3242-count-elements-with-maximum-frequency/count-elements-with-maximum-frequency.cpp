15f99edfc850cfcffb15b2c2bc48be533213abee
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-03-15 00:55:04 +0530

    Time: 3 ms (82.85%) | Memory: 22.1 MB (49.51%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -4,7 +4,7 @@ public:
     {
         vector<int> hashh(100,0);
         int tmp=-1,ans=0;
-        for(int &it:nums)
+        for(int it:nums)
         {
             ++hashh[it-1];
             tmp=max(hashh[it-1],tmp);
