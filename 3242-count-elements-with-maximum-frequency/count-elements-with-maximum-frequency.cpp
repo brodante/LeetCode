e49237c106abc3dd7f95ea5892efb0fd5a8fe643
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-03-15 00:54:07 +0530

    Time: 4 ms (64.28%) | Memory: 21.9 MB (72.34%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums)
     {
-        int hashh[100],tmp=-1,ans=0;
+        vector<int> hashh(100,0);
+        int tmp=-1,ans=0;
         for(int &it:nums)
         {
             ++hashh[it-1];
