a413983a7f3aa782dd156a16e09a4c5cc31b44cb
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-03-20 18:11:12 +0530

    Time: 178 ms (61.67%) | Memory: 98.2 MB (13.44%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -12,27 +12,22 @@ class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2)
     {
-        ListNode *head1 = list1;
-        ListNode *tmp = list1;
-        ListNode *head2 = list2;
+        ListNode *head1=list1;
+        ListNode *tmp=list1;
+        ListNode *head2=list2;
         // Traverse b nodes from the beginning of list1 to find tmp
-        for (int i = 0; i < b; ++i)
-        {
-            tmp = tmp->next;
-        }
+        for(int i=0;i<b;++i)
+            tmp=tmp->next;
         // Traverse a-1 nodes from the beginning of list1 to find head1
-        for (int i = 0; i < a - 1; ++i) {
-            head1 = head1->next;
-        }
+        for(int i=0;i<a-1;++i)
+            head1=head1->next;
         // Connect the a-1th node of list1 to the head of list2
-        head1->next = head2;
+        head1->next=head2;
         // Traverse list2 to find its tail
-        while (head2->next != nullptr)
-        {
-            head2 = head2->next;
-        }
-        // Connect the ath node of list1 to the tail of list2
-        head2->next = tmp->next;
+        while(head2->next!=nullptr)
+            head2=head2->next;
+        // Connect the end of list2 to the tmp(bth node) of list1
+        head2->next=tmp->next;
         return list1;
     }
 };
