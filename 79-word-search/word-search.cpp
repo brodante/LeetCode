c3f57eb8b9443139ed90e0483ca0f056c64a6dea
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-03 14:57:13 +0530

    Time: 528 ms (59.60%) | Memory: 10.7 MB (65.03%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,29 +1,26 @@
 class Solution {
 private:
-    bool dfs(int r, int c, int i, vector<vector<char>>& b, string& w)
+    bool dfs(int r, int c, int i, vector<vector<char>> &b, string &w, vector<vector<bool>> &hashh)
     {
         if(i==w.size())
             return true;
-        if(r<0 || c<0 || r>=b.size() || c>=b[0].size() || b[r][c]!=w[i])
+        if(r<0 || c<0 || r>=b.size() ||c>=b[0].size() || b[r][c]!=w[i] || hashh[r][c])
             return false;
-        char temp=b[r][c];
-        b[r][c] = '*'; // Mark as visited
-        
-        bool ans=(dfs(r+1, c, i+1, b, w) ||
-                  dfs(r-1, c, i+1, b, w) ||
-                  dfs(r, c+1, i+1, b, w) ||
-                  dfs(r, c-1, i+1, b, w));
-        
-        b[r][c] = temp; // Restore
-        
+        hashh[r][c]=true;
+        bool ans = (dfs(r+1, c, i+1, b, w, hashh) ||
+                    dfs(r-1, c, i+1, b, w, hashh) || 
+                    dfs(r, c+1, i+1, b, w, hashh) || 
+                    dfs(r, c-1, i+1, b, w, hashh));
+        hashh[r][c]=false;
         return ans;
     }
 public:
-    bool exist(vector<vector<char>>& b, string& w)
+    bool exist(vector<vector<char>>& b, string &w)
     {
+        vector<vector<bool>> hashh(b.size(),vector<bool>(b[0].size(),false));
         for(int i=0;i<b.size();++i)
             for(int j=0;j<b[0].size();++j)
-                if (dfs(i,j,0,b,w))
+                if(dfs(i,j,0,b,w,hashh))
                     return true;
         return false;
     }
