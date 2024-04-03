b798913a95dde7a5941c3744c6addd0c60b563ee
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-03 14:26:13 +0530

    Time: 190 ms (93.23%) | Memory: 9.5 MB (96.96%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,31 +1,30 @@
 class Solution {
 private:
-    bool dfs(int r, int c, int i, vector<vector<char>>& b, string& w) {
-        if (i == w.size())
+    bool dfs(int r, int c, int i, vector<vector<char>>& b, string& w)
+    {
+        if(i==w.size())
             return true;
-        if (r < 0 || c < 0 || r >= b.size() || c >= b[0].size() || b[r][c] != w[i])
+        if(r<0 || c<0 || r>=b.size() || c>=b[0].size() || b[r][c]!=w[i])
             return false;
+        char temp=b[r][c];
+        b[r][c] = '*'; // Mark as visited
         
-        char temp = b[r][c]; // Store the current character before modification
-        b[r][c] = '*'; // Mark the cell as visited
+        bool ans=(dfs(r+1, c, i+1, b, w) ||
+                  dfs(r-1, c, i+1, b, w) ||
+                  dfs(r, c+1, i+1, b, w) ||
+                  dfs(r, c-1, i+1, b, w));
         
-        bool ans = (dfs(r + 1, c, i + 1, b, w) ||
-                    dfs(r - 1, c, i + 1, b, w) ||
-                    dfs(r, c + 1, i + 1, b, w) ||
-                    dfs(r, c - 1, i + 1, b, w));
-        
-        b[r][c] = temp; // Restore the original character
+        b[r][c] = temp; // Restore
         
         return ans;
     }
 public:
-    bool exist(vector<vector<char>>& b, string& w) {
-        for (int i = 0; i < b.size(); ++i) {
-            for (int j = 0; j < b[0].size(); ++j) {
-                if (dfs(i, j, 0, b, w))
+    bool exist(vector<vector<char>>& b, string& w)
+    {
+        for(int i=0;i<b.size();++i)
+            for(int j=0;j<b[0].size();++j)
+                if (dfs(i,j,0,b,w))
                     return true;
-            }
-        }
         return false;
     }
 };
