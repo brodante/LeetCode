bf27a2473fa6dc96d4a8701ff2c2733b6365d484
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-04 12:56:54 +0530

    Time: 209 ms (19.86%) | Memory: 98.9 MB (9.88%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int carFleet(int t, vector<int>& p, vector<int>& s)
     {
-        map<int, float, greater<int>> mpp;
+        //map<int, float, greater<int>> mpp;
+        map<int, float> mpp;
         for(int i=0;i<p.size();++i)
-            mpp[p[i]]=(float)(t-p[i])/s[i];
+            mpp[-p[i]]=(float)(t-p[i])/s[i]; //mpp[p[i]]=(float)(t-p[i])/s[i]; -p[i] for rev order
         int ans=0;
         float cur=0;
         for(pair<int, float> it:mpp)
