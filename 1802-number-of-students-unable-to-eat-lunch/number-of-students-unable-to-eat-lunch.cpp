93c33ae542bac28728129da347336147f9e3c245
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-08 10:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     int countStudents(vector<int>& st, vector<int>& sd)
     {
-        deque<int> q(st.begin(),st.end());
-        int i=0,cnt=0;
-        while(!q.empty())
+        int ans=st.size();
+        vector<int>hashh(2);
+        hashh[0]=count(st.begin(),st.end(),0);
+        hashh[1]=ans-hashh[0];
+
+        for(int i:sd)
         {
-            int curr=q.front();
-            q.pop_front();
-            if(curr!=sd[i])
+            if(hashh[i]>0)
             {
-                ++cnt;
-                q.push_back(curr);
+                --ans;
+                --hashh[i];
             }
             else
-            {
-                ++i;
-                cnt=0;
-            }
-            if(cnt==q.size())
-                break;
+                return ans;
         }
-        return q.size();
+        return 0;
     }
 };
