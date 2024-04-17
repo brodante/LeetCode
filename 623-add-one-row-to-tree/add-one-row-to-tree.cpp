1534ada742dcb4ddde837bd137734907e3ed78c8
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-17 23:45:52 +0530

    Time: 16 ms (32.74%) | Memory: 24.3 MB (74.27%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -11,9 +11,9 @@
  */
 class Solution {
 private:
-    TreeNode *bfs(TreeNode*root, int val, int depth, int curr)
+    TreeNode *spsc(TreeNode *root, int val, int depth, int curr)
     {
-        if(root==nullptr)
+        if(!root)
             return nullptr;
         if(curr==depth-1)
         {
@@ -23,14 +23,13 @@ private:
             root->left= new TreeNode(val);
             root->right= new TreeNode(val);
 
-            root->left->left=leftTemp;
-            root->right->right=rightTemp;
+            root->left->left= leftTemp;
+            root->right->right= rightTemp;
 
             return root;
         }
-        root->left= bfs(root->left,val,depth,curr+1);
-        root->right= bfs(root->right,val,depth,curr+1);
-
+        root->left=spsc(root->left, val, depth,curr+1);
+        root->right=spsc(root->right, val, depth,curr+1);
         return root;
     }
 public:
@@ -42,7 +41,6 @@ public:
             tmp->left=root;
             return tmp;
         }
-        int curr=1; //current depth = 1 cuz root
-        return bfs(root, val, depth, curr);
+        return spsc(root, val, depth, 1);
     }
 };
