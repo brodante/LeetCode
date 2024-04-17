87781d0f68563fdc220f3bf335488af8b31e41e9
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-17 23:24:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (17.64%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -11,17 +11,17 @@
  */
 class Solution {
 private:
-    int dfs(TreeNode *root, bool isLeft)
+    int spsc(TreeNode *root, bool isLeft)
     {
-        if(root==nullptr)
+        if(!root)
             return 0;
-        if(root->left==nullptr && root->right==nullptr) // if node has no child nodes
-            return isLeft==1?root->val:0;   // if the node is LEFT node
-        return dfs(root->left,true) + dfs(root->right,false); 
+        if(!root->left&&!root->right)
+            return isLeft==true?root->val:0;
+        return spsc(root->left, true) + spsc(root->right, false);
     }
 public:
     int sumOfLeftLeaves(TreeNode* root)
     {
-        return dfs(root,false);
+        return spsc(root, false);
     }
 };
