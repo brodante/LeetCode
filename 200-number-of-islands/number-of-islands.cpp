1cafb28ec9d2c3cd8e81dce938b6750a01313cc0
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-19 23:27:16 +0530

    Time: 33 ms (33.65%) | Memory: 15.9 MB (62.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,15 +1,15 @@
 class Solution
 {
 private:
-    void removeIsland(int i, int j, vector<vector<char>> &tmp)
+    void removeIsland(int i, int j, vector<vector<bool>> &tmp)
     {
         int r=tmp.size();
         int c=tmp[0].size();
 
-        if(i<0||i==r||j<0||j==c||tmp[i][j]=='0')
+        if(i<0||i==r||j<0||j==c||tmp[i][j]==0)
             return ;
         
-        tmp[i][j]='0';
+        tmp[i][j]=0;
 
         removeIsland(i, j+1, tmp); //connected islands go brrrrrr
         removeIsland(i, j-1, tmp);
@@ -23,11 +23,16 @@ public:
         int c=grid[0].size();
         int ans=0;
 
-        vector<vector<char>> tmp=grid; //copy of grid we can delete islands from tmp
+        vector<vector<bool>> tmp(r,vector<bool>(c,0)); //copy of grid we can delete islands from tmp
 
         for(int i=0;i<r;++i)
             for(int j=0;j<c;++j)
-                if(tmp[i][j]=='1')
+                if(grid[i][j]=='1')
+                    tmp[i][j]=1;
+
+        for(int i=0;i<r;++i)
+            for(int j=0;j<c;++j)
+                if(tmp[i][j]==1)
                 {
                     ans+=1;
                     removeIsland(i,j,tmp);
