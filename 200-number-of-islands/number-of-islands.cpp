e7d362a9c497bb28d63f233b706a0d028681e401
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-19 23:21:14 +0530

    Time: 28 ms (62.85%) | Memory: 16 MB (62.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,36 @@
-class Solution {
+class Solution
+{
 private:
-    int bfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& mpp)
+    void removeIsland(int i, int j, vector<vector<char>> &tmp)
     {
-        deque<pair<int,int>> q;
-        mpp[i][j]=true;
-        q.push_front({i,j});
-        while(!q.empty())
-        {
-            pair<int,int> rc=q.front();
-            q.pop_front();
-            int x = rc.first;
-            int y = rc.second;
-            vector<pair<int,int>> directions={{-1,0},{1,0},{0, 1},{0,-1}}; //left, right, up, down
-            for(pair<int,int> &it:directions)
-            {
-                int nx= x+ it.first;
-                int ny= y+ it.second;
-                if(nx>=0 && nx<grid.size() && ny>=0 && ny<grid[0].size() && grid[nx][ny]=='1' && !mpp[nx][ny])
-                {
-                    mpp[nx][ny]=true;
-                    q.push_back({nx, ny});
-                }
-            }
-        }
-        return 1;
+        int r=tmp.size();
+        int c=tmp[0].size();
+
+        if(i<0||i==r||j<0||j==c||tmp[i][j]=='0')
+            return ;
+        
+        tmp[i][j]='0';
+
+        removeIsland(i, j+1, tmp);
+        removeIsland(i, j-1, tmp);
+        removeIsland(i+1, j, tmp);
+        removeIsland(i-1, j, tmp);
     }
 public:
     int numIslands(vector<vector<char>>& grid)
     {
         int r=grid.size();
         int c=grid[0].size();
-
-        vector<vector<bool>> mpp(r,vector<bool>(c,0));
-
         int ans=0;
+
+        vector<vector<char>> tmp=grid; //copy of grid we can delete islands from tmp
 
         for(int i=0;i<r;++i)
             for(int j=0;j<c;++j)
-                if(grid[i][j]=='1'&&mpp[i][j]==false)
+                if(tmp[i][j]=='1')
                 {
-                    bfs(i,j,grid,mpp);
-                    ++ans;
-                    //or ans +=bfs(i, j, grid, mpp);
+                    ans+=1;
+                    removeIsland(i,j,tmp);
                 }
         return ans;
     }
