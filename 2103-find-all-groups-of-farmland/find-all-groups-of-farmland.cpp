72470f3b2032b8f6942f01dbcc907ca66f5ab215
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-20 23:19:30 +0530

    Time: 1586 ms (5.09%) | Memory: 88 MB (36.73%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+void dfs(int i, int j, vector<vector<bool>> &tmp, int &a1, int &a2)
+    {
+
+        int r=tmp.size();
+        int c=tmp[0].size();
+
+        if(i<0||i==r||j<0||j==c||tmp[i][j]==0)
+            return;
+        
+        a1=max(a1,i);
+        a2=max(a2,j);
+
+        tmp[i][j]=0;
+
+        dfs(i, j+1, tmp, a1, a2); //connected farm lands go brrrrrr X again lmao
+        dfs(i, j-1, tmp, a1, a2);
+        dfs(i+1, j, tmp, a1, a2);
+        dfs(i-1, j, tmp, a1, a2);
+    }
+public:
+    vector<vector<int>> findFarmland(vector<vector<int>>& land)
+    {
+        int r=land.size();
+        int c=land[0].size();
+        vector<vector<int>> ans;
+
+        vector<vector<bool>> tmp(r,vector<bool>(c,0)); //copy of grid we can delete farm lands from tmp
+
+        for(int i=0;i<r;++i)
+            for(int j=0;j<c;++j)
+                if(land[i][j]==1)
+                    tmp[i][j]=1;
+
+        for(int i=0;i<r;++i)
+            for(int j=0;j<c;++j)
+                if(tmp[i][j]==1)
+                {
+                    int a1=i,a2=j; //s
+                    dfs(i,j,tmp,a1,a2);
+                    ans.push_back({i,j,a1,a2});
+                }
+        return ans;
+    }
+}; 
