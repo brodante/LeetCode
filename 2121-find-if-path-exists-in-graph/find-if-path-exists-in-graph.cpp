ca889ae11bf373086c457aa4b3674583f91868c3
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-21 22:33:57 +0530

    Time: 607 ms (38.25%) | Memory: 187.6 MB (50.47%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,29 +1,31 @@
 class Solution {
-private:
-    bool dfs(unordered_map<int,vector<int>> &mpp,int &src, int &dst, vector<bool> &vis)
-    {
-        if(src==dst)
-            return true;
-        if(vis[src]==true)
-            return false;
-        vis[src]=true;
-        for(int it:mpp[src])
-        {
-            if(dfs(mpp,it,dst,vis)==true)
-                return true;
-        }
-        return false;
-    }
 public:
     bool validPath(int n, vector<vector<int>>& edges, int src, int dst)
     {
-        unordered_map<int,vector<int>> mpp;
+        unordered_map<int,vector<int>> mpp; //adjacency list
         for(int i=0;i<edges.size();++i)
         {
             mpp[edges[i][0]].push_back(edges[i][1]);
             mpp[edges[i][1]].push_back(edges[i][0]);
         }
         vector<bool> vis(n,false);
-        return dfs(mpp,src,dst,vis);
+        queue<int> q;
+        q.push(src);
+        while(!q.empty())
+        {
+            int tmp=q.front();
+            q.pop();
+            if(tmp==dst)
+                return true;
+            for(int it:mpp[tmp])
+            {
+                if(!vis[it])
+                {
+                    q.push(it);
+                    vis[it]=true;
+                }
+            }
+        }
+        return false;
     }
 };
