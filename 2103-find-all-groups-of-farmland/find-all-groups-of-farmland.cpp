a105ce824cf4e92bfa271cdd879d01a9f1d324d2
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-21 09:00:22 +0530

    Time: 107 ms (63.75%) | Memory: 70.4 MB (68.93%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-void dfs(int i, int j, vector<vector<bool>> &tmp, int &a1, int &a2)
+void dfs(int i, int j, vector<vector<int>> &tmp, int &a1, int &a2)
     {
 
         int r=tmp.size();
@@ -25,19 +25,19 @@ public:
         int c=land[0].size();
         vector<vector<int>> ans;
 
-        vector<vector<bool>> tmp(r,vector<bool>(c,0)); //copy of grid we can delete farm lands from tmp
+        //vector<vector<bool>> tmp(r,vector<bool>(c,0)); //copy of grid we can delete farm lands from tmp
+
+        //for(int i=0;i<r;++i)
+        //    for(int j=0;j<c;++j)
+        //        if(land[i][j]==1)
+        //            tmp[i][j]=1;
 
         for(int i=0;i<r;++i)
             for(int j=0;j<c;++j)
                 if(land[i][j]==1)
-                    tmp[i][j]=1;
-
-        for(int i=0;i<r;++i)
-            for(int j=0;j<c;++j)
-                if(tmp[i][j]==1)
                 {
                     int a1=i,a2=j; //s
-                    dfs(i,j,tmp,a1,a2);
+                    dfs(i,j,land,a1,a2);
                     ans.push_back({i,j,a1,a2});
                 }
         return ans;
