f3af8dd3d41053d0710726182311a5d8d09b0461
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-24 23:35:54 +0530

    Time: 134 ms (36.65%) | Memory: 63.1 MB (38.62%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n == 1)
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges)
+    {
+        if(n==1)
             return {0};
-        
-        vector<int> result;
+
+        unordered_map<int,vector<int>> adj;
         vector<int> indegree(n);
-        map<int, vector<int>> adj;
-        
-        for(vector<int> vec:edges) {
-            int u = vec[0];
-            int v = vec[1];
-            indegree[u]++;
-            indegree[v]++;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+
+        for(vector<int> &it:edges) //adjacency list
+        {
+            int u=it[0];
+            int v=it[1];
+            ++indegree[u];
+            ++indegree[v];
+            adj[u].emplace_back(v);
+            adj[v].emplace_back(u);
         }
-        
-        queue<int> que;
-        for(int i = 0; i<n; i++) {
-            if(indegree[i] == 1) //adding all leafs node so that we can remove them later
-                que.push(i);
-        }
-        
-        while(n > 2) {
-            int size = que.size();
-            n -= size; //removing nodes with indegreee 1
-            
-            while(size--) {
-                int u = que.front();
-                que.pop();
-                for(int v:adj[u]) {
-                    indegree[v]--;
-                    if(indegree[v] == 1)
-                        que.push(v);
+
+        vector<int> ans;
+        queue<int> q;
+
+        for(int i=0;i<n;++i)
+            if(indegree[i]==1)
+                q.push(i); //pushing leaf nodes into queue for bfs
+        while(n>2)
+        {
+            int size=q.size();
+            n-=size;
+            while(size--)
+            {
+                int u=q.front();
+                q.pop();
+                for(int &it:adj[u])
+                {
+                    --indegree[it];
+                    if(indegree[it]==1)
+                    {
+                        q.push(it);
+                    }
                 }
             }
         }
-        
-        while(!que.empty()) {
-            result.push_back(que.front());
-            que.pop();
-        }
-        return result;
+        while(!q.empty())
+            ans.emplace_back(q.front()),q.pop();
+        return ans;
     }
 };
