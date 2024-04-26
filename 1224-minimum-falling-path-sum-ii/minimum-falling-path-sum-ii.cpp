141a90ae69fd515fa0a9dbe3930d59731273dbca
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-26 23:47:52 +0530

    Time: 22 ms (93.37%) | Memory: 16.9 MB (86.55%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -3,14 +3,14 @@ public:
     int minFallingPathSum(vector<vector<int>>& grid)
     {
         int n=grid.size();
-        int firstmin=0, secondmin=0, pos=-1;
+        int firstmin=0, secondmin=0, pos=-1; //if we initialise INT_MAX "line add" will give overflow
         for(int i=0;i<n;++i)
         {
             int firstmin2=INT_MAX, secondmin2=INT_MAX, pos2=-1;
             for(int j=0;j<n;++j)
             {
                 int tmp= (j!=pos) ? firstmin:secondmin;
-                if(grid[i][j]+tmp< firstmin2)
+                if(grid[i][j]+tmp< firstmin2) //line add
                 {
                     secondmin2= firstmin2;
                     firstmin2= grid[i][j]+ tmp;
