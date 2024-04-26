eab33754b985737295accf18d5f9e446f8ad5fe9
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-26 23:41:21 +0530

    Time: 29 ms (83.63%) | Memory: 16.9 MB (86.55%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
-private:
-    int helper(int r, int c,vector<vector<int>> &tmp, vector<vector<int>>& grid)
-    {
-        int n=grid.size();
-        if(r==n-1)
-            return grid[r][c];
-        if(tmp[r][c]!=0)
-            return tmp[r][c];
-        int ans=INT_MAX;
-
-        for(int j=0;j<n;++j)
-        {
-            if(c!=j)
-                ans=min(ans,grid[r][c]+helper(r+1,j,tmp,grid));
-        }
-        tmp[r][c]=ans;
-        return ans;
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& grid)
     {
         int n=grid.size();
-        int ans=INT_MAX;
-        vector<vector<int>> tmp(n,vector<int>(n));
-        for(int j=0;j<n;++j)
-            ans=min(ans,helper(0,j,tmp,grid));
-        return ans;
+        int firstmin=0, secondmin=0, pos=-1;
+        for(int i=0;i<n;++i)
+        {
+            int firstmin2=INT_MAX, secondmin2=INT_MAX, pos2=-1;
+            for(int j=0;j<n;++j)
+            {
+                int tmp= (j!=pos) ? firstmin:secondmin;
+                if(grid[i][j]+tmp< firstmin2)
+                {
+                    secondmin2= firstmin2;
+                    firstmin2= grid[i][j]+ tmp;
+                    pos2= j;
+                }
+                else
+                    secondmin2= min(secondmin2,grid[i][j]+ tmp);
+            }
+            firstmin= firstmin2;
+            secondmin= secondmin2;
+            pos= pos2;
+        }
+        return firstmin;
     }
 };
