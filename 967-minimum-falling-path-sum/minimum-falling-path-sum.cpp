865b98515dc8954115842efdd9577ee517afc9ec
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-26 22:18:54 +0530

    Time: 11 ms (71.80%) | Memory: 12.9 MB (92.79%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,19 +3,19 @@ public:
     int minFallingPathSum(vector<vector<int>>& m)
     {
         int n=m.size();
-        for(int i=1;i<n;++i)
+        for(int i=n-2;i>=0;--i)
         {
             for(int j=0;j<n;++j)
             {
-                int mid=m[i-1][j];
-                int left=j>0?m[i-1][j-1]:INT_MAX;
-                int right=j<(n-1)?m[i-1][j+1]:INT_MAX;
+                int mid=m[i+1][j];
+                int left=j>0?m[i+1][j-1]:INT_MAX;
+                int right=j<(n-1)?m[i+1][j+1]:INT_MAX;
                 m[i][j]+= min(min(mid,left),right);
             }
         }
         int ans=INT_MAX;
         for(int j=0;j<n;++j)
-            ans=min(ans,m[n-1][j]);
+            ans=min(ans,m[0][j]);
         return ans;
     }
 };
