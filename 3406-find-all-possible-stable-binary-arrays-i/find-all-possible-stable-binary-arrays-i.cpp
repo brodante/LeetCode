14bb1a5307266ba04b9c8c04b0a24f2c37aa8d32
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-28 04:09:51 +0530

    Time: 261 ms (22.22%) | Memory: 44 MB (11.11%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfStableArrays(int x, int y, int l)
+    {
+        const int MOD=1e9+7;
+        vector<vector<vector<int>>> dp(y+1, vector<vector<int>>(x+1, vector<int>(2,0)));
+        dp[0][0][0]= dp[0][0][1]= 1;
+        for(int i=0;i<=y;++i)
+        {
+            for(int j=0;j<=x;++j)
+            {
+                for(int k=1;k<=l;++k)
+                {
+                    if(i-k>=0)
+                        dp[i][j][1]= (dp[i][j][1]+ dp[i-k][j][0])% MOD;
+                    if (j-k>=0)
+                        dp[i][j][0]= (dp[i][j][0]+ dp[i][j-k][1])% MOD;
+                }
+            }
+        }
+        return (dp[y][x][0]+dp[y][x][1])>MOD?(dp[y][x][0]+dp[y][x][1])%MOD:(dp[y][x][0]+dp[y][x][1]);
+        
+    }
+};
