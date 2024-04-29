d4ed1990c30d08b4797102b05ccbf098760956b6
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-30 04:40:25 +0530

    Time: 778 ms (5.16%) | Memory: 113.4 MB (17.59%) - LeetSync

--- 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -1,34 +1,52 @@
 class Solution {
 private:
-    vector<unordered_set<int>> tree;
-    vector<int> res, count;
-    void dfs(int root, int pre) {
-        for (auto i : tree[root]) {
-            if (i == pre) continue;
-            dfs(i, root);
-            count[root] += count[i];
-            res[root] += res[i] + count[i];
+    long base_result=0;
+    vector<int> subtreecount;
+    int N;
+    int dfsBase(unordered_map<int, vector<int>> &adj, int curr_node, int prev_node, int curr_depth)
+    {
+        int total_count=1;
+        base_result+=curr_depth;
+        
+        for(int &it:adj[curr_node])
+        {
+            if(it==prev_node)
+                continue;
+            total_count+=dfsBase(adj, it, curr_node, curr_depth+1);
         }
-    }
+        subtreecount[curr_node]=total_count;
 
-    void dfs2(int root, int pre) {
-        for (auto i : tree[root]) {
-            if (i == pre) continue;
-            res[i] = res[root] - count[i] + count.size() - count[i];
-            dfs2(i, root);
+        return total_count;
+    }
+    void dfs(unordered_map<int, vector<int>> &adj, int parent_node, int prev_node, vector<int> &ans)
+    {
+        for(int &it:adj[parent_node])
+        {
+            if(it==prev_node)
+                continue;
+            ans[it]=ans[parent_node]-subtreecount[it]+(N-subtreecount[it]);
+            dfs(adj, it, parent_node,ans);
         }
     }
 public:
-    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
-        tree.resize(N);
-        res.assign(N, 0);
-        count.assign(N, 1);
-        for (auto e : edges) {
-            tree[e[0]].insert(e[1]);
-            tree[e[1]].insert(e[0]);
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges)
+    {
+        unordered_map<int, vector<int>> adj;
+        N=n;
+        subtreecount.assign(n,0);
+        for(vector<int> &it:edges)
+        {
+            adj[it[0]].emplace_back(it[1]);
+            adj[it[1]].emplace_back(it[0]);
         }
-        dfs(0, -1);
-        dfs2(0, -1);
-        return res;
+        base_result=0;
+
+        dfsBase(adj, 0,-1, 0);
+
+        vector<int> ans(n,0);
+        ans[0]=base_result;
+
+        dfs(adj, 0, -1, ans);
+        return ans;
     }
 };
