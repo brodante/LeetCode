291bfea24920d14ac4c414472df197f5790c76cf
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-04-30 04:59:23 +0530

    Time: 88 ms (97.01%) | Memory: 91.1 MB (98.59%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -2,8 +2,17 @@ class Solution {
 public:
     int minOperations(vector<int>& n, int k)
     {
+        int tmp=0;
         for(int it:n)
-            k^=it;
-        return __builtin_popcount(k);
+            tmp^=it;
+        int ans=0;
+        while(k!=0 || tmp!=0)
+        {
+            if((k%2)!=(tmp%2))
+                ++ans;
+            k/=2;
+            tmp/=2;
+        }
+        return ans;
     }
 };
