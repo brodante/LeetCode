255bc10d6249a96885e18d42a27b455ad94b7986
Surya Pratap Singh Chauhan <44796607+brodante@users.noreply.github.com>
2024-08-01 21:12:27 +0530

    Time: 3 ms (91.87%) | Memory: 17.6 MB (16.71%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details)
+    {
+        int ans=0;
+        for(auto it:details)
+            {
+                int age=(it[11]-'0')*10;
+                age+=(it[12]-'0');
+                if(age>60)
+                    ++ans;
+            }
+        return ans;
+    }
+};
